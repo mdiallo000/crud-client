868886a40a2f2f62c6a74d9da0f35dad18f6a618
mdiallo98 <mamadou1237@gmail.com>
2022-01-31 05:19:18 -0500

    styles and css

--- src/Info.jsx
+++ src/Info.jsx
@@ -1,18 +1,21 @@
 import React from "react";
-
+import "./Info.css";
 function Info() {
 	return (
-		<div>
-			<label htmlFor='name'>Name:</label>
-			<input type='text' name='name' id='' />
-			<label htmlFor='age'>Age:</label>
-			<input type='number' name='age' id='' />
-			<label htmlFor='country'>Country:</label>
-			<input type='text' name='country' id='' />
-			<label htmlFor='positon'>Positon:</label>
-			<input type='text' name='positon' id='' />
-			<label htmlFor='wage'>Wage(year):</label>
-			<input type='number' name='wage' id='' />
+		<div className='container'>
+			<form className='form-container' action=''>
+				<label htmlFor='name'>Name:</label>
+				<input type='text' name='name' id='' />
+				<label htmlFor='age'>Age:</label>
+				<input type='number' name='age' id='' />
+				<label htmlFor='country'>Country:</label>
+				<input type='text' name='country' id='' />
+				<label htmlFor='positon'>Positon:</label>
+				<input type='text' name='positon' id='' />
+				<label htmlFor='wage'>Wage(year):</label>
+				<input type='number' name='wage' id='' />
+				<button className='btn'>Submit</button>
+			</form>
 		</div>
 	);
 }
